2040d1922fa5b36bc29e13f5c4c60c8d2743e379
João Henrique Costa <88861391+Joao19921@users.noreply.github.com>
2023-02-15 20:39:20 -0300

    Update DesafioQuitandaDoSeuZe.java

--- DesafioQuitandaDoSeuZe.java
+++ DesafioQuitandaDoSeuZe.java
@@ -1,26 +1,38 @@
 package Desafio;
-
 import java.util.*;
 
 public class DesafioQuitandaDoSeuZe {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-		int morangos = input.nextInt();
-		//int macas = input.nextInt();
-		morangos = input.nextInt();
-		//macas = input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int morangos = input.nextInt();
+        int macas = input.nextInt();
+        double tot1 = 0;
+        double tot2 = 0;
+        double valor = 0;
+        double kilo = 0;
+        double desc = 0;
+        if (morangos <= 5) {
+            tot1 = morangos * 2.50;
+        }else {
+            tot1 = morangos * 2.20;
+        }
 
-		if (morangos > 5) {
-			morangos *= 2.20;
-		} else if (morangos <= 5) {
-			morangos *= 2.50;
-		}
+        if (macas <= 5) {
+            tot2 = macas * 1.80;
+        }else {
+            tot2 = macas * 1.50;
+        }
+        kilo = morangos + macas;
+        valor = tot1 + tot2;
 
-		System.out.println(morangos);
-
-		input.close();
-
-	}
+        if(valor > 25.00 || kilo > 8){
+            desc = valor * 10 /100;
+            System.out.println(valor-desc);
+        }else {
+            System.out.println(valor);
+        }
+        input.close();
+    }
 }
